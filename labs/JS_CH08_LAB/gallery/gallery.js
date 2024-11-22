7bb8633607c3c17fee826942e4fc7b2444f412e1
carson-young18 <Carson_Young@insideranken.org>
2024-11-22 08:42:38 -0600

    js ch8

--- labs/JS_CH08_LAB/gallery/gallery.js
+++ labs/JS_CH08_LAB/gallery/gallery.js
@@ -1,7 +1,21 @@
 "use strict";
 
 $(document).ready(() => {
-  
+  $(document).on("keydown", evt => {
+    const links = $("a");
+    let currentLink = links.index($(document.activeElement));
+
+    if (evt.key == "ArrowRight"){
+      evt.preventDefault();
+      links[(currentLink + 1) % links.length].focus();
+    }
+    else if (evt.key == "ArrowLeft"){
+      evt.preventDefault();
+      links[(currentLink - 1 + links.length) % links.length].focus();
+    }
+  });
+
+
   $("a").each((index, link) => {
     const image = new Image();
     image.src = link.href;
@@ -10,9 +24,14 @@ $(document).ready(() => {
 
   $("a").click( evt => {
     evt.preventDefault();
+  }); 
+
+  $("a").focus( evt => {
     const link = evt.currentTarget;
     
     $("#displayImage").attr("src", link.href);
     $("#imageCaption").text(link.title);
-  })
+  });
+
+  $("#first").focus();
 });
