0c17660e1744362148169ffc62af03c8f5b3006e
carson-young18 <Carson_Young@insideranken.org>
2024-12-09 11:48:25 -0600

    final project

---
+++ final-project/final-project/src/AddStudent.jsx
@@ -0,0 +1,90 @@
+import { nanoid } from 'nanoid';
+import React, {useState} from 'react'
+
+const AddStudent = (props) => {
+  const [firstName, setFirstName] = useState('');
+  const [lastName, setLastName] = useState('');
+  const [email, setEmail] = useState('');
+  const [gradYear, setGradYear] = useState('');
+  const [selectedFile, setSelectedFile] = useState('');
+
+  const doWork = () => {
+    const newStudent = {
+      'id' : nanoid(),
+      'firstName' : firstName,
+      'lastName' : lastName,
+      'email' : email,
+      'gradYear' : gradYear,
+      'image' : URL.createObjectURL(selectedFile)
+    }
+
+    props.addStudent(newStudent)
+  }
+
+  const imageUpdate = (event) => {
+    setSelectedFile(event.target.files[0]);
+  }
+
+  return (
+    <div className='row'>
+      <div className='col-md-2'>
+        <label htmlFor="txtFirstName" className='form-label'>First Name</label>
+        <input 
+          type="text" 
+          name="txtFirstName" 
+          id="txtFirstName" 
+          placeholder='First Name' 
+          className='form-control' 
+          onChange={(event) => setFirstName(event.currentTarget.value)} 
+          value={firstName}
+        />
+      </div>
+      <div className='col-md-2'>
+        <label htmlFor="txtLastName" className='form-label'>Last Name</label>
+        <input 
+          type="text" 
+          name="txtLastName" 
+          id="txtLastName" 
+          placeholder='Last Name' 
+          className='form-control'
+          onChange={(event) => setLastName(event.currentTarget.value)} 
+          value={lastName}
+        />
+      </div>
+      <div className='col-md-2'>
+        <label htmlFor="txtEmail" className='form-label'>Email Address</label>
+        <input 
+          type="email" 
+          name="txtEmail" 
+          id="txtEmail" 
+          placeholder='Email Address' 
+          className='form-control'
+          onChange={(event) => setEmail(event.currentTarget.value)} 
+          value={email}
+        />
+      </div>
+      <div className='col-md-2'>
+        <label htmlFor="txtGradYear" className='form-label'>Grduation Year</label>
+        <input 
+          type="text" 
+          name="txtGradYear" 
+          id="txtGradYear" 
+          placeholder='Graduation Year' 
+          className='form-control'
+          onChange={(event) => setGradYear(event.currentTarget.value)} 
+          value={gradYear}
+        />
+      </div>
+      <div className='col-md-2'>
+        <label htmlFor="fileUpload" className='form-label'>Student Image</label>
+        <input type="file" name="fileUpload" id="fileUpload" className='form-control' onChange={imageUpdate}/>
+      </div>
+      <div className='col-md-4'>
+        <button type="button" className='btn btn-success btn-lg' id='btnAdd' onClick={doWork}/>
+      </div>
+      
+    </div>
+  )
+}
+
+export default AddStudent
--- final-project/final-project/src/App.jsx
+++ final-project/final-project/src/App.jsx
@@ -1,25 +1,105 @@
 import { useState } from 'react'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import './App.css'
+import { nanoid } from 'nanoid'
+import AddStudent from './AddStudent';
 
 function App() {
+
+  const [allStudents, setAllStudents] = useState(null);
+
+  const students = [{
+    id: nanoid(),
+    firstName: "Demetria",
+    lastName: "Dolden",
+    email: "ddolden0@sun.com",
+    image: 'images/student1.jpg'
+  }, {
+    id: nanoid(),
+    firstName: "Alejandra",
+    lastName: "Yurmanovev",
+    email: "ayurmanovev1@blogger.com",
+    image: 'images/student2.jpg'
+  }, {
+    id: nanoid(),
+    firstName: "Karla",
+    lastName: "Leiden",
+    email: "kleiden2@cisco.com",
+    image: 'images/student3.jpg'
+  }, {
+    id: nanoid(),
+    firstName: "Kendal",
+    lastName: "Sell",
+    email: "ksell3@latimes.com",
+    image: 'images/student4.jpg'
+  }, {
+    id: nanoid(),
+    firstName: "Audry",
+    lastName: "Lasslett",
+    email: "alasslett4@topsy.com",
+    image: 'images/student5.jpg'
+  }, {
+    id: nanoid(),
+    firstName: "Forrest",
+    lastName: "Sarchwell",
+    email: "fsarchwell5@storify.com",
+    image: 'images/student6.jpg'
+  }, {
+    id: nanoid(),
+    firstName: "Elmore",
+    lastName: "Abdie",
+    email: "eabdie6@dion.ne.jp",
+    image: 'images/student7.jpg'
+  }, {
+    id: nanoid(),
+    firstName: "Griff",
+    lastName: "Blaszkiewicz",
+    email: "gblaszkiewicz7@a8.net",
+    image: 'images/student8.jpg'
+  }, {
+    id: nanoid(),
+    firstName: "Aggy",
+    lastName: "Foard",
+    email: "afoard8@kickstarter.com",
+    image: 'images/student9.jpg'
+  }, {
+    id: nanoid(),
+    firstName: "Perceval",
+    lastName: "Tipens",
+    email: "ptipens9@noaa.gov",
+    image: 'images/student10.jpg'
+  }];
+
+  const addStudent = (newStudent) => {
+    const updatedStudents = [...allStudents, newStudent]
+    saveStudents(updatedStudents);
+  }
 
   return (
     <div className='container'>
       <div className='row'>
-        <div className='col-lg-2'>
-          <ul className='list-group list-group-flush'>
-            <li className='list-group-item'>One</li>
-            <li className='list-group-item'>Two</li>
-            <li className='list-group-item'>Three</li>
-          </ul>
-        </div>
-        <div className='col-lg-2'>Col2</div>
-        <div className='col-lg-2'>Col3</div>
-        <div className='col-lg-2'>Col4</div>
-        <div className='col-lg-2'>Col5</div>
-        <div className='col-lg-2'>Col6</div>
+        {allStudents && allStudents.map((student) => (
+          <div className='col-lg-2' key={student.id}>
+            <div className='card'>
+              <img src={student.image} alt="Our student" />
+              <ul className='list-group list-group-flush'>
+                <li className='list-group-item'>{student.firstName}</li>
+                <li className='list-group-item'>{student.lastName}</li>
+                <li className='list-group-item'>{student.email}</li>
+              </ul>
+            </div>
+          </div>
+        ))}
+        <div className='col-lg-2'></div>
+        <div className='col-lg-2'></div>
+        <div className='col-lg-2'></div>
+        <div className='col-lg-2'></div>
+        <div className='col-lg-2'></div>
       </div>
+
+      {!allStudents && <button type='button' className='btn btn-large btn-success' onClick={() => setAllStudents(students)}>Save Students</button>}
+    
+      <AddStudent addStudent={addStudent}/>
     </div>
   )
 }
